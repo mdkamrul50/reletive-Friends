a840c41d8d6cb14c46fea8649167770873016c29
mdkamrul50 <kamrul0088e@gmail.com>
2026-04-14 20:27:37 +0600

    Enhance FriendInfo component with detailed friend display and styling

--- src/app/data/[friendId]/page.jsx
+++ src/app/data/[friendId]/page.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
 import friends from '@/friendsData/friends.json';
+import Image from 'next/image';
 
 const FriendInfo = async ({ params }) => {
   const { friendId } = await params;
   const friend = friends.find((friendIn) => friendIn.id == friendId);
   console.log("this is friend" ,friend);
   return (
-    <div>
-      <h2>{friendId}</h2>
+    <div className="bg-gray-100 py-20">
+      <div className="container mx-auto flex gap-10">
+        <div className="py-7 px-22 bg-white shadow-sm flex justify-center items-center text-center rounded-xl ">
+          <div className="space-y-2">
+            <div className="flex justify-center">
+              <Image
+                className="rounded-full"
+                src={friend.picture}
+                height={120}
+                width={120}
+                alt="img"
+              />
+            </div>
+            <h2 className="text-xl font-bold ">{friend.name}</h2>
+
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="badge badge-soft badge-success rounded-full pb-1 font-semibold">
+                {friend.tags[1]}
+              </div>
+              <p
+                className={`badge  rounded-full text-white pb-1 ${friend.status === 'overdue' ? 'badge-error' : 'badge-warning'} ${friend.status === 'on-track' && 'bg-green-800'}`}
+              >
+                {friend.status}
+              </p>
+            </div>
+            <p className="text-xl italic font-semibold text-gray-400">
+              "{friend.tags[0]}, {friend.tags[1]}"
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex gap-6">
+            <div className="shadow-sm rounded-xl bg-white text-center p-10 space-y-2">
+              <span className="text-3xl font-bold text-green-800">
+                {friend.days_since_contact}
+              </span>
+              <p className="text-xl font-semibold text-gray-400">
+                Days Since Contact
+              </p>
+            </div>
+            <div className="shadow-sm rounded-xl bg-white text-center p-10 space-y-2">
+              <span className="text-3xl font-bold text-green-800">
+                {friend.goal}
+              </span>
+              <p className="text-xl font-semibold text-gray-400">Goal (Days)</p>
+            </div>
+            <div className="shadow-sm rounded-xl bg-white text-center p-10 space-y-3 ">
+              <span className="text-3xl font-bold text-green-800">
+                Feb 27, 2026
+              </span>
+              <p className="text-xl font-semibold text-gray-400">
+                Days Since Contact
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
